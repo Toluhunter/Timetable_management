20a7f28d5a6d09a52e2a865b0d1f9c3c636cb03d
toluhunter <toluhunter19@gmail.com>
2021-11-16 15:06:21 +0100

    frontend serverd

---
+++ pages/views.py
@@ -0,0 +1,14 @@
+from django.views import generic
+from django.http import HttpResponse
+
+# Create your views here.
+class HomepageView(generic.TemplateView):
+    template_name = "index.html"
+
+class AboutPageView(generic.TemplateView):
+    template_name = "about.html"
+
+class ConatactPageView(generic.TemplateView):
+
+    def get(self, request):
+        return HttpResponse("We await ini")
