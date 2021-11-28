a69c7f6277a2c1778adee0dd8a802fce540e7eed
toluhunter <toluhunter19@gmail.com>
2021-11-28 01:09:29 +0100

    Create Table functionality

--- timetable/models.py
+++ timetable/models.py
@@ -1,26 +1,34 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
 from accounts.models import (
     Courses,
     Lecturer,
     Department,
     Venue
 )
+
 # Create your models here.
 class Table(models.Model):                                                                                                                                                            
     course_code = models.ForeignKey(Courses, models.CASCADE, db_column='course_code', blank=True, null=True)                                                                       
     lecturer = models.ForeignKey(Lecturer, models.SET_NULL, blank=True, null=True)
     venue_id = models.ForeignKey(Venue, models.SET_NULL, blank=True, null=True)
-    class_start_time = models.TimeField()
-    class_end_time = models.TimeField()
+    start_time = models.TimeField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.course_code} {self.start_time}"
+
+   
 
 class TimeTable(models.Model):
     days = [
-        ('Monday', 'Monday'),
-        ('Tuesday', 'Tuesday'),
-        ('Wenesday', 'Wenesday'),
-        ('Thursday', 'Thursday'),
-        ('Friday', 'Friday')
+        ('monday', 'Monday'),
+        ('tuesday', 'Tuesday'),
+        ('wednesday', 'Wednesday'),
+        ('thursday', 'Thursday'),
+        ('friday', 'Friday')
     ]
     levels = [
         ("100", "100 Level"),
@@ -32,3 +40,6 @@ class TimeTable(models.Model):
     table = models.ForeignKey(Table, on_delete=models.SET_NULL, blank=True, null=True)
     day = models.CharField(max_length=10, choices=days)
     level = models.CharField(max_length=3, choices=levels)
+    
+    def __str__(self):
+        return f"{self.day} {self.department}"
