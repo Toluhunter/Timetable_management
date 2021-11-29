11796d7a13482d17c1cde668f800b7ac5da8e459
toluhunter <toluhunter19@gmail.com>
2021-11-29 17:11:01 +0100

    Timetable view functionality

--- timetable/views.py
+++ timetable/views.py
@@ -1,11 +1,20 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.views.generic.base import TemplateView
 from .mixins import IsAdminMixin
 from django.views import generic
 from django.urls import reverse
 from .forms import SelectTimetableForm, CreateTimetableForm
-from accounts.models import Courses, Department, Lecturer, Admin, Venue
+from accounts.models import (
+    Courses, 
+    Department, 
+    Lecturer, 
+    Admin, 
+    Venue
+)
 from .save_timetable import SaveTimeTable
+from .models import TimeTable
+
 # Create your views here.
 
 class SelectTimeTableView(IsAdminMixin, generic.FormView):
@@ -45,13 +54,51 @@ class CreateTimeTableView(IsAdminMixin, generic.FormView):
         level = self.kwargs["level"]
         return SaveTimeTable.save(form, department=department, level=level)
 
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super().get_form_kwargs(**kwargs)
         kwargs.update({
             'department':self.kwargs["department"],
             'level':self.kwargs["level"]
         })
         return kwargs
     
-class TimeTableView(generic.TemplateView):
+class TimeTableView(LoginRequiredMixin, generic.TemplateView):
     template_name = "timetable.html"
+
+    def get_context_data(self, **kwargs):
+        label = "Level" 
+        user = self.request.user
+        if user.is_student:
+            label = "Lecturer"
+            monday = TimeTable.objects.filter(day="monday", department=user.department, level=user.student_set.level)
+            tuesday = TimeTable.objects.filter(day="tuesday", department=user.department, level=user.student_set.level)
+            wednesday = TimeTable.objects.filter(day="wednesday", department=user.department, level=user.student_set.level)
+            thursday = TimeTable.objects.filter(day="thursday", department=user.department, level=user.student_set.level)
+            friday = TimeTable.objects.filter(day="friday", department=user.department, level=user.student_set.level)
+        else:
+            monday = TimeTable.objects.filter(day="monday", department=user.department)
+            tuesday = TimeTable.objects.filter(day="tuesday", department=user.department)
+            wednesday = TimeTable.objects.filter(day="wednesday", department=user.department)
+            thursday = TimeTable.objects.filter(day="thursday", department=user.department)
+            friday = TimeTable.objects.filter(day="friday", department=user.department)
+
+        kwargs = super().get_context_data(**kwargs)
+        days = [
+            "monday",
+            "tuesday",
+            "wednesday",
+            "thursday",
+            "friday"
+        ]
+        kwargs.update(
+            {
+                "days":days,
+                "label":label,
+                "monday":monday,
+                "tuesday":tuesday,
+                "wednesday":wednesday,
+                "thursday":thursday,
+                "friday":friday
+            }
+        )
+        return kwargs
