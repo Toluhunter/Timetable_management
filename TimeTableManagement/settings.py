8019a713a9b087363ee8aaf32078d5af732b67f3
toluhunter <toluhunter19@gmail.com>
2021-12-10 11:17:49 +0100

    Updated Pipfile

--- TimeTableManagement/settings.py
+++ TimeTableManagement/settings.py
@@ -143,6 +143,7 @@ STATICFILES_DIRS = [ BASE_DIR / "static" ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
