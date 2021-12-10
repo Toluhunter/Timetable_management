62edc2b95130a13eceadfcfc2e39bb7a2ad85fb5
toluhunter <toluhunter19@gmail.com>
2021-12-10 11:10:33 +0100

    Added staticfile storage

--- TimeTableManagement/settings.py
+++ TimeTableManagement/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / "static" ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
